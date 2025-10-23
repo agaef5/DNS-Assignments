961d036a1c1737195415f3f0f8f4c6d4d145947b
Aga Furgalska <aga.furgalska@wp.pl>
2025-10-23 08:48:44 +0200

    commmit
    Took 15 seconds

--- Entities/obj/Debug/netcoreapp10.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/netcoreapp10.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7368868bc4a33ebf881663390ac924296566beaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06939aede18cfb69c8625ebb7978969e0561a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FileRepositories/obj/Debug/net10.0/FileRepositories.AssemblyInfo.cs
+++ FileRepositories/obj/Debug/net10.0/FileRepositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileRepositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7368868bc4a33ebf881663390ac924296566beaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06939aede18cfb69c8625ebb7978969e0561a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileRepositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileRepositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
